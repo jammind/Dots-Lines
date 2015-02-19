2ad768f38e7af62aa93996aff8537abec9fda73b
jammind <jammind@gmail.com>
2015-02-19 19:44:30 +0800

    offset & color attributes added to Lines.plains

--- lines.js
+++ lines.js
@@ -1,9 +1,9 @@
 Lines = {
-    GRID_DISTANCE: 15,
-    PLANE_DISTANCE: 1000,
-    OFFSET_X: -300,
-    OFFSET_Y: 300,
-    OFFSET_Z: 500
+    GRID_DISTANCE: 5,
+    PLANE_DISTANCE: 400,
+    OFFSET_X: -100,
+    OFFSET_Y: 100,
+    OFFSET_Z: 0
 };
 
 Lines.MATERIAL = new THREE.LineBasicMaterial( {
@@ -17,122 +17,115 @@ Lines.MATERIAL = new THREE.LineBasicMaterial( {
 
 
 
-Lines.bitmaps = [
+Lines.plains = [
 
-//    'xxxxxxxxxxxxx',
-
-//    '      x      /' +
-//    '      x      /' +
-//    '      x      /' +
-//    '      x      /' +
-//    '      x      /' +
-//    '      x      /' +
-//    '      x      /' +
-//    '      x      /' +
-//    '      x      /' +
-//    '      x      /' +
-//    '      x      /' +
-//    '      x      /' +
-//    '      x      /',
-
-    '          x                     x          /' +
-    '          x                     x          /' +
-    '           x                   x           /' +
-    '           x                   x           /' +
-    '            x                 x            /' +
-    '            x                 x            /' +
-    '             x               x             /' +
-    '             x               x             /' +
-    ' xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /' +
-    '                     x                     /'
-    
+    {
+        color: 0xff00ff,
+        offsetX: 20,
+        offsetY: -20,
+        bitmap:
+            '          x                     x          /' +
+            '          x                     x          /' +
+            '           x                   x           /' +
+            '           x                   x           /' +
+            '            x                 x            /' +
+            '            x                 x            /' +
+            '             x               x             /' +
+            '             x               x             /' +
+            ' xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /' +
+            '                     x                     /'
+    }
     ,
-    
-    '    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx   /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx   /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx   /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    x                 x                    /' +
-    '    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/' +
-    '                                          x/' +
-    '                                          x/' +
-    '                                          x/' +
-    '                                          x/' +
-    '                                          x/' +
-    '  x         x         x         x         x/' +
-    '  x         x         x         x         x/' +
-    '  x         x         x         x         x/' +
-    '  x         x         x         x         x/' +
-    '  x         x         x         x         x/' +
-    '  x         x         x         x         x/' +
-    '  x         x         x         x         x/' +
-    '  x         x         x         x         x/' +
-    '                                          x/' +
-    '                                          x/' +
-    '                                          x/' +
-    '                                     xxxxxx/'
-
+    {
+        color: 0x00ffff,
+        offsetX: -20,
+        offsetY: 20,
+        bitmap:
+        '    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx   /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx   /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx   /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    x                 x                    /' +
+        '    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/' +
+        '                                          x/' +
+        '                                          x/' +
+        '                                          x/' +
+        '                                          x/' +
+        '                                          x/' +
+        '  x         x         x         x         x/' +
+        '  x         x         x         x         x/' +
+        '  x         x         x         x         x/' +
+        '  x         x         x         x         x/' +
+        '  x         x         x         x         x/' +
+        '  x         x         x         x         x/' +
+        '  x         x         x         x         x/' +
+        '  x         x         x         x         x/' +
+        '                                          x/' +
+        '                                          x/' +
+        '                                          x/' +
+        '                                     xxxxxx/'
+    }
 
 ];
 
-Lines.parseBitmap = function(bitmaps) {
+Lines.parseBitmap = function(plains) {
 
     var arrays = [];
 
-    for (var indexBitmap in bitmaps) {
+    for (var i in plains) {
 
-        var bitmap = bitmaps[indexBitmap];
+        var bitmap = plains[i].bitmap;
         var array = [];
         var i = 0, a = 0, b = 0;
 
@@ -149,10 +142,10 @@ Lines.parseBitmap = function(bitmaps) {
                     break;
 
                 default:
-//                                if (!array[a]) {
-//                                    array[a] = [];
-//                                }
-//                                array[a][b] = 1;
+//                    if (!array[a]) {
+//                        array[a] = [];
+//                    }
+//                    array[a][b] = 1;
                     array.push({x:a, y:b});
                     a ++;
                     break;
@@ -172,40 +165,63 @@ Lines.parseBitmap = function(bitmaps) {
 // Draw lines
 Lines.drawLines = function (scene) {
     
-    var arrays = Lines.parseBitmap(Lines.bitmaps);
+    var arrays = Lines.parseBitmap(Lines.plains);
     Lines.parentTransform = new THREE.Object3D();
     
     var totalNodes = Math.max(arrays[0].length,arrays[1].length);
     for ( var n=0; n<totalNodes ; n++) {
 
-        var geometry3 = new THREE.Geometry(),
-            points = hilbert2D( new THREE.Vector3( 0,0,0 ), 400.0, 4, 0, 1, 2, 3, 4, 5, 6, 7 ),
-            colors3 = [];
-
+        var geometry3 = new THREE.Geometry();
+        var colors3 = [];
+        var offsetX = 0, offsetY = 0;
+        
+        // Node A
+        if (Lines.plains[0].offsetX) {
+            offsetX = Lines.plains[0].offsetX;
+        }
+        if (Lines.plains[0].offsetY) {
+            offsetY = Lines.plains[0].offsetY;
+        }
+//        console.log('Offset A', offsetX, offsetY);
         var a = Math.floor(n / totalNodes * arrays[0].length);
         var p0 = arrays[0][a];
-        var x0 = p0.x * Lines.GRID_DISTANCE + Lines.OFFSET_X;
-        var y0 = - p0.y * Lines.GRID_DISTANCE + Lines.OFFSET_Y;
-        var z0 = Lines.OFFSET_Z;
+        var x0 = (p0.x + offsetX) * Lines.GRID_DISTANCE + Lines.OFFSET_X;
+        var y0 = - (p0.y + offsetY) * Lines.GRID_DISTANCE + Lines.OFFSET_Y;
+        var z0 = Lines.PLANE_DISTANCE + Lines.OFFSET_Z;
+        
+        colors3.push(new THREE.Color( Lines.plains[0].color ));
+        geometry3.vertices.push( {x:x0, y:y0, z:z0} );
+        
+        // Node Black inthe Middle
+        geometry3.vertices.push( {x:(x0+x1)*.5, y:(y0+y1)*.5, z:(z0+z1)*.5} );
+        colors3.push(new THREE.Color( 0x000000 ));
 
+        // Node B
+        offsetX = 0;
+        offsetY = 0;
+        if (Lines.plains[1].offsetX) {
+            offsetX = Lines.plains[1].offsetX;
+        }
+        if (Lines.plains[1].offsetY) {
+            offsetY = Lines.plains[1].offsetY;
+        }
+//        console.log('Offset B', offsetX, offsetY);
         var b = Math.floor(n / totalNodes * arrays[1].length);
-//        console.log(a, b);
         var p1 = arrays[1][b];
-        var x1 = p1.x * Lines.GRID_DISTANCE + Lines.OFFSET_X;
-        var y1 = - p1.y * Lines.GRID_DISTANCE + Lines.OFFSET_Y;
+        var x1 = (p1.x + offsetX) * Lines.GRID_DISTANCE + Lines.OFFSET_X;
+        var y1 = - (p1.y + offsetY) * Lines.GRID_DISTANCE + Lines.OFFSET_Y;
         var z1 = - Lines.PLANE_DISTANCE + Lines.OFFSET_Z;
 
         geometry3.vertices.push( {x:x1, y:y1, z:z1} );
-        geometry3.vertices.push( {x:x0, y:y0, z:z0} );
+        colors3.push(new THREE.Color( Lines.plains[1].color ));
 
-        var color = new THREE.Color( 0xffffff );
-        color.setHSL( a / points.length, 1.0, 0.5 );
-        colors3.push(new THREE.Color( 0xff0000 ));
-        colors3.push(new THREE.Color( 0x0000ff ));
-        geometry3.colors = colors3;
+//        var color = new THREE.Color( 0xffffff );
+//        color.setHSL( a / points.length, 1.0, 0.5 );
+        
 
         var line, p, scale = 0.3, d = 225;
 
+        geometry3.colors = colors3;
         line = new THREE.Line(geometry3, Lines.MATERIAL);
         line.scale.x = line.scale.y = line.scale.z =  scale*1.5;
         line.position.x = 0;
